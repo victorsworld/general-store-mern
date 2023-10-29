c66f791bfb2065d629b2e3d69fc3c687ea2bfbad
victorsworld <victordirect48@gmail.com>
2023-10-29 17:59:02 -0400

    logout add

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,12 +2,33 @@ import React from 'react';
 import Login from '../Pages/Login';
 import Register from '../Pages/Register';
 import { Link, useNavigate } from 'react-router-dom';
+import { removeUserToken } from '../Auth/authLocalStorage';
 
-const NavBar = () => {
+const NavBar = ({
+  user,
+  isVerified,
+  setRefreshToken,
+  setUser,
+  setIsVerified,
+}) => {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    setRefreshToken(true);
+    const resultLogout = await removeUserToken();
+    if (resultLogout) {
+      setRefreshToken(false);
+      setUser(null);
+      setIsVerified(false);
+      navigate('/home');
+      console.log('logged out');
+    }
+  };
+
   return (
     <div>
       <div className="">
-        <Link to="home">Home</Link>{' '} <Link to="login">Login</Link>{' '}
+        <Link to="home">Home</Link> <Link to="login">Login</Link>{' '}
+        <button onClick={handleLogout}>Logout</button>{' '}
         <Link to="register">Register</Link>{' '}
       </div>
     </div>
